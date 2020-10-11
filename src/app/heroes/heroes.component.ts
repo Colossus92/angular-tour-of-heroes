5ffd5f7f5bb1c6e98079b524f3d9c302ea02c699
Calvin Richardson <calvin.richardson@sogeti.com>
2020-10-11 11:54:05 +0200

    Refactor to get heroes from HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
 import {Hero} from "../hero";
 
 @Component({
@@ -9,16 +10,21 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
